61356e5cafb8173f79896bdb00f38de9436f1cc3
Kola Serko <kol4serko@gmail.com>
2021-12-17 03:40:11 -0800

    Add project files.

---
+++ ConsoleApp1/CsvParcer.cs
@@ -0,0 +1,64 @@
+﻿using CsvHelper;
+using Cyrillic.Convert;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class CsvParcer
+    {
+        public string GetManagerSurname(string filePath)
+        {
+            var fileName = Path.GetFileName(filePath);
+            return fileName.Substring(0, fileName.Length - 13);
+        }
+        public List<CsvDataContainer> GetDataFromCsv(string filePath)
+        {
+            using var reader = new StreamReader(filePath);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var records = csv.GetRecords<CsvDataContainer>();
+            return records.ToList();
+        }
+
+        public List<CsvDataContainerNew> ProcessData(List<CsvDataContainer> data)
+        {
+            var conversion = new Conversion();
+            var groupedData = data.GroupBy(x => x.Code);
+            var result = new List<CsvDataContainerNew>();
+
+            foreach (var group in groupedData)
+            {
+                var s = group.ToList();
+
+                var model = new CsvDataContainerNew() { Code = group.Key, NameList = new List<string>() };
+
+                foreach (var item in group.ToList())
+                {
+                    var translatedText = conversion.RussianCyrillicToLatin(item.Name).Trim().Replace(" ","_");
+                    model.NameList.Add(translatedText);
+                }
+
+                result.Add(model);
+            }
+
+            Console.WriteLine();
+            return result;
+        }
+
+        public List<CsvDataContainer> ProcessDataNew(List<CsvDataContainer> data)
+        { 
+            var conversion = new Conversion();
+
+            foreach (var item in data)
+            {
+                item.Name = conversion.RussianCyrillicToLatin(item.Name).Trim().Replace(" ", "_");
+            }
+           
+            return data;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+Console.WriteLine("Start");
+
+var parcer = new CsvParcer();
+
+var data = parcer.GetDataFromCsv("C:/Users/Mikalay/Desktop/dat.csv");
+
+var records= parcer.ProcessData(data);
+var recordsNew = parcer.ProcessDataNew(data);
+
+var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+    // Don't write the header again.
+    HasHeaderRecord = false,
+};
+using (var stream = File.Open("C:/Users/Mikalay/Desktop/dat1.csv", FileMode.Append))
+using (var writer = new StreamWriter(stream))
+using (var csv = new CsvWriter(writer, config))
+{
+    csv.WriteRecords(records);
+}
+
+Console.WriteLine("erewddgdfg");
