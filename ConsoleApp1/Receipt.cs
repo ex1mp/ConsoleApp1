f3a0546d5b553e6717ebb7faae7cb3c93090596a
Kola Serko <kol4serko@gmail.com>
2021-12-18 21:12:50 +0300

    fix naming

---
+++ ConsoleApp1/Receipt.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    public class Receipt
+    {
+        public int Code { get; set; }
+
+        public List<string> Products { get; set; }
+
+        public string Date { get; set; }
+
+        public string CashКegister { get; set; }
+    }
+}
